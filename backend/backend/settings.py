d559f59e283f882df663386ac7554635d0d6e1a7
Andrey Orlov <yasei3009@gmail.com>
2023-08-12 22:34:38 +0300

    update files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,14 +1,14 @@
 import os
 from pathlib import Path
 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.getenv('SECRET_KEY', 'default_key')
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+DEBUG = os.getenv('DEBUG') == 'True'
 
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.3.109', '127.0.0.1', 'localhost', 'yaseiwebproject.myvnc.com']
 
 
 # Application definition
